2f7cc38f7de5e203a43355286cf97cec97a8923b
Michael Welborn <michael.welborn@indicodata.ai>
2025-05-08 11:46:59 -0500

    Fix errors in `utils.get()` and `utils.has()`

--- indico_toolkit/results/utils.py
+++ indico_toolkit/results/utils.py
@@ -17,19 +17,19 @@ def get(result: object, value_type: "type[Value]", *keys: "str | int") -> Value:
                 raise KeyError(f"{key!r} not in {result.keys()!r}")
         elif isinstance(result, list):
             if isinstance(key, int):
-                if 0 >= key < len(result):
+                if 0 <= key < len(result):
                     result = result[key]
                 else:
-                    raise IndexError(f"list index {key} out of range {len(result)}")
+                    raise IndexError(f"{key} out of range [0,{len(result)})")
             else:
-                TypeError(f"list cannot be indexed with {key!r}")
+                raise TypeError(f"list can't be indexed with {key!r}")
         else:
-            TypeError(f"{type(result)} cannot be traversed")
+            raise TypeError(f"{type(result)} can't be traversed")
 
     if isinstance(result, value_type):
         return result
     else:
-        raise TypeError(f"value `{result!r}` does not have expected type {value_type}")
+        raise TypeError(f"value `{result!r}` doesn't have type {value_type}")
 
 
 def has(result: object, value_type: "type[Value]", *keys: "str | int") -> bool:
@@ -37,9 +37,9 @@ def has(result: object, value_type: "type[Value]", *keys: "str | int") -> bool:
     Check if `result` can be traversed using `keys` to a value of type `value_type`.
     """
     for key in keys:
-        if isinstance(key, str) and isinstance(result, dict) and key in result:
+        if isinstance(result, dict) and key in result:
             result = result[key]
-        elif isinstance(key, int) and isinstance(result, list) and key < len(result):
+        elif isinstance(result, list) and isinstance(key, int) and 0 <= key < len(result):  # fmt: skip  # noqa: E501
             result = result[key]
         else:
             return False
